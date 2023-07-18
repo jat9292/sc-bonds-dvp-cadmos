b119c500770298565fc35a2e7c511e6245423342
NBFinanceTech <nassibj.boueri@gmail.com>
2023-07-18 13:25:38 +0200

    Fake Curve Pool

---
+++ src/CurveExecutor.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+// SATURN project (last updated v0.1.0)
+
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Cash.sol";
+
+interface ICurveDeposit{
+  function exchange(
+    int128 i,
+    int128 j,
+    uint256 dx,
+    uint256 min_dy
+  ) external;
+      function get_dx(
+    int128 i,
+    int128 j,
+    uint256 dy
+  ) external view returns (uint256);
+}
+
+contract CashTokenExecutor is Ownable{
+
+    address immutable eurToken;
+    address immutable usdToken;
+    int128 constant i = 0; // identifier of buyerSourceCashToken in Pool Curve (entry of forex operation) --> EUR
+    int128 constant j = 1; // identifier of payment cashToken in Pool Curve (output of forex operation) --> USD
+    address immutable poolCurve; // Mariana/Pool  Curve  address
+    address immutable dvp; //Address of DVP smart contract
+
+        /* ========== CONSTRUCTOR ========== */
+
+    constructor(address eurToken_, address usdToken_, address poolCurve_, address dvp_) {
+        eurToken = eurToken_;
+        usdToken = usdToken_;
+        poolCurve = poolCurve_;
+        dvp = dvp_;
+    }
+
+
+    function requestTransfer(
+        address cashToken,
+        address _buyer,
+        address _seller,
+        uint256 settlementAmount
+    ) external {
+        require(msg.sender == dvp, "unAuthorized");
+        require(cashToken==usdToken, "unsupported currency");
+
+        uint256 dx = ICurveDeposit(poolCurve).get_dx(i, j, settlementAmount);
+        Cash(eurToken).transferFrom(
+                            _buyer,
+                            address(this),
+                            dx
+                        );
+        ICurveDeposit(poolCurve).exchange(i, j, dx, settlementAmount); 
+
+
+        Cash(eurToken).transferFrom(
+                            address(this),
+                            _seller,
+                            settlementAmount
+                        );
+    }
+}
---
+++ src/FakeCurvePool.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+// SATURN project (last updated v0.1.0)
+
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Cash.sol";
+
+
+
+contract FakeCurvePool {
+
+    address immutable eurToken;
+    address immutable usdToken;
+    address immutable chfToken;
+    uint256 wad = 1e18;
+    uint256 constant eurusd = 110*1e16; //1.10
+        /* ========== CONSTRUCTOR ========== */
+
+    constructor(address eurToken_, address usdToken_, address chfToken_) {
+        eurToken = eurToken_;
+        usdToken = usdToken_;
+        chfToken = chfToken_;
+    }
+
+
+    function get_dx(
+        int128 i,
+        int128 j,
+        uint256 dy
+    ) external view returns (uint256){
+        require(i==0, " Trade not supported by FakePoolCurve");
+        require(j==1, " Trade not supported by FakePoolCurve");
+        uint256 dx = dy*wad/eurusd;
+        return dx;
+
+    } 
+
+
+function exchange(
+    int128 i,
+    int128 j,
+    uint256 dx,
+    uint256 min_dy
+  ) external {
+        require(i==0, " Trade not supported by FakePoolCurve");
+        require(j==1, " Trade not supported by FakePoolCurve");
+        uint256 dy = dx*eurusd/wad;
+        require(dy>=min_dy, " Unsufficient output amount");
+        Cash(eurToken).transferFrom(
+                            msg.sender,
+                            address(this),
+                            dx
+                        );
+        Cash(usdToken).transferFrom(
+                            address(this),
+                            msg.sender,
+                            dy
+                        );
+  }
+
+}
+
+
